78d63fbfdf43b577466657c832d9678712bd07b6
satzapp <satzapplications@gmail.com>
2021-09-25 07:43:21 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  additionForm: FormGroup;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initilizeForm();
+  }
+
+  initilizeForm(): void {
+    this.additionForm = this.fb.group({
+      jobRole: '',
+      jobExperience: 0,
+      prefferedLocation: this.fb.group({
+        india: true,
+        usa: false,
+        uk: false,
+      }),
+      experiences: this.fb.array([
+        this.fb.group({
+          company: '',
+          experience: 0,
+        }),
+      ]),
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.additionForm);
+  }
+
+  addExp(): void {
+    this.experiences.push(this.fb.control(''));
+  }
+
+  removeExp(index: number): void {
+    this.experiences.removeAt(index);
+  }
+
+  get experiences(): FormArray {
+    return this.additionForm.get('experiences') as FormArray;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, ReactiveFormsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
